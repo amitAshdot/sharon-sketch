c8db8e5fc32722f2b5d175feb06bf751bdf31a8b
Amit Ashdot <amitashdot@gmail.com>
2022-12-30 01:43:49 -0600

    mobile new design

--- js/index.js
+++ js/index.js
@@ -274,4 +274,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 5000)
     // ----- END SHOWING POPUP -----
 
+
+    new PerformanceObserver((list) => {
+        const latestEntry = list.getEntries().at(-1);
+
+        if (latestEntry?.element?.getAttribute('loading') == 'lazy') {
+            console.warn('Warning: LCP element was lazy loaded', latestEntry);
+        }
+    }).observe({ type: 'largest-contentful-paint', buffered: true });
 }, { passive: true });
