18d7e89ac8468dc9f4e5b670525b88fe93b8e9f8
Amit Ashdot <amitashdot@gmail.com>
2022-12-30 13:46:56 -0600

    add pwa

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var headers = new Headers();
+    headers.append('Service-Worker-Allowed', '/');
+    // console.log(headers.get('Service-Worker-Allowed'));
+
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+            navigator.serviceWorker
+                .register("sw.js")
+                // .then(res => console.log("service worker registered"))
+                .catch(err => console.log("service worker not registered", err))
+        })
+    }
+
+
     // When the user scrolls the page, execute myFunction
     window.onscroll = function () { setFixed() };
     let header = document.getElementById("header");
---
+++ sw.js
@@ -0,0 +1,45 @@
+const casheName = "integer-byte -site-v1";
+const assets = [
+    "/",
+    "index.html",
+    "css/index.css",
+    "css/index.min.css",
+    "js/app.js",
+    "js/jquery3.6.min.js",
+    "js/owl.carousel.min.js",
+    "images/favicon-32x32.png",
+    "images/favicon-16x16.png",
+    "images/logo/BOOMERANG-LOGO-FIN-W-.webp",
+    "images/logo/BOOMERANG-LOGO-FIN-M-.webp",
+    "images/logo/BOOMERANG-LOGO-FIN-S-.webp",
+    "images/floating/BOOMERANG-PRICE-BACK-_1_-_1_.webp",
+    "images/floating/My-project.webp",
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(casheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== assets)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+});
