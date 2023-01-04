fd3be240cc07cd08aaf9c94f5e8f6ed6a697f320
Amit Ashdot <amitashdot@gmail.com>
2023-01-03 18:03:21 -0700

    add carousel

--- js/app.js
+++ js/app.js
@@ -227,32 +227,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // -----START CAROUSEL-----
-    // $(".slider").slick({
-    //     infinite: false,
-    //     rtl: true,
-    //     infinite: true,
-    //     centerMode: true,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 2,
-    //     responsive: [
-    //         //     {
-    //         //     breakpoint: 1024,
-    //         //     settings: {
-    //         //         slidesToShow: 3,
-    //         //         infinite: true
-    //         //     }
-    //         // },
-    //         {
-    //             breakpoint: 600,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 dots: true
-    //             }
-    //         }, {
-    //             breakpoint: 300,
-    //             settings: "unslick" // destroys slick
-    //         }]
-    // });
+    $(".slider").slick({
+        rtl: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+            //     {
+            //     breakpoint: 1024,
+            //     settings: {
+            //         slidesToShow: 3,
+            //         infinite: true
+            //     }
+            // },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }, {
+                breakpoint: 300,
+                settings: "unslick" // destroys slick
+            }]
+    });
 
     // -----END CAROUSEL-----
 
