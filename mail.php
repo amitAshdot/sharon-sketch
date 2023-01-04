7761c169f0c7b2bd4218f1e02575fc0df6510208
Amit Ashdot <amitashdot@gmail.com>
2023-01-03 17:42:24 -0700

    rmv gtm

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $message = " \n מברוק! קיבלתם ליד חדש \r\n<br>
 ";
 $pagetitle = "ליד חדש - partnertv-sales.co.il";
 $headers = "Content-Type: text/html; charset=UTF-8";
-$from = "newlead@partner-tv.co";
+$from = "Boomerangfit23@gmail.com";
 
 // header("Location: https://partnertv-sales.co.il/thankyoupage/");
 // mail($recepient, $pagetitle, $message, $headers,$from);
