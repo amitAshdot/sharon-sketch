c2204484e2055097f54871ff682dd28f9c4a08f5
Amit Ashdot <amitashdot@gmail.com>
2023-01-18 18:57:17 +0200

    change mailphp

--- mail.php
+++ mail.php
@@ -1,53 +1,43 @@
+<?PHP
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+
+// require './php/src/Exception.php';
+// require './php/src/PHPMailer.php';
+// require './php/src/SMTP.php';
 
 
 
-<?php
-if(isset($_POST['email'])) {
-  $email_to = "amitashdot@gmail.com";
-  $email_subject = "Summarized propose of the email";
-  //Errors to show if there is a problem in form fields.
-  function died($error) {
-      echo "We are sorry that we can procceed your request due to error(s)";
-      echo "Below is the error(s) list <br /><br />";
-      echo $error."<br /><br />";
-      echo "Please go back and fix these errors.<br /><br />";
-      die();
-  }
-  // validation expected data exists
-  if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['phone'])) {
-      died('We are sorry to proceed your request due to error within form entries');   
-  }
-  $first_name = $_POST['name']; // required
-  $email_from = $_POST['email']; // required
-    $telephone = $_POST['phone']; // not required
-  $error_message = "";
-  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {
-  $error_message .= 'You entered an invalid email<br />';
-  }
-  $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$first_name)) {
-  $error_message .= 'Invalid first name<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-  $email_message = "Form details below.\n\n";
-  function clean_string($string) {
-    $bad = array("content-type","bcc:","to:","cc:","href");
-    return str_replace($bad,"",$string);
-  }
-  $email_message .= "First Name: ".clean_string($first_name)."\n";
+$email = $_POST["email"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
 
-  $email_message .= "Email: ".clean_string($email_from)."\n";
-  $email_message .= "Telephone: ".clean_string($telephone)."\n";
-  // create email headers
-  $headers = 'From: '.$email_from."\r\n".
-  'Reply-To: '.$email_from."\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers);
-  }
+$to = "amitashdot@gmail.com";
+$subject = "כההההה, יש ליד חדש";
+$headers = "From: no-reply@boomerang.fit\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
 
+$message = " \n כהההה, מישהו רוצה לשמוע על האימונים\r\n<br>
+שם: $name \r\n  
+טלפון: $phone \r\n
+email: $email \r\n
+
+בהצלחה!
+";
+
+$user = $email;
+$usersubject = "כהההה";
+$userheaders = "From: no-reply@boomerang.fit\n";
+$usermessage = "תודה פשוש";
+
+$retval = mail($to,$subject,$message,$headers); //send to Sharon&Aya
+if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+mail($user,$usersubject,$usermessage,$userheaders); //send to user
 ?>
+
